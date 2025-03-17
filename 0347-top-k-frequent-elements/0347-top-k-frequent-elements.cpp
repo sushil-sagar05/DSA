95885834a54200f1a46b25a823b471a4f8a5a75f
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-17 08:36:45 +0530

    Time: 8 ms (10.64%), Space: 19.8 MB (10.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-     vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int,int>mp;
-        for(int num : nums){
+        for(int &num : nums){
             mp[num]++;
         }
-        vector<pair<int,int>>freq(mp.begin(),mp.end());
-       sort(freq.begin(),freq.end(),[](pair<int,int>&a,pair<int,int>&b){
-        return a.second>b.second;
-       });
-       vector<int>result;
-       for (int i = 0; i < k; i++)
-       {
-        result.push_back(freq[i].first);
+        vector<vector<int>>bucket(n+1);
+        //freq = i
+        //element = value
+        //bucket[i]=element having i frequency 
+       for(auto &it :mp){
+         int element = it.first;
+         int freq = it.second;
+         bucket[freq].push_back(element);
        }
-       return result;
-       
+       //result
+       //pick element from right to left from bucket to find max frequency of element
+       vector<int>result;
+        for(int i=n;i>=0;i--){
+            if(bucket[i].size()==0) continue;
+            while(bucket[i].size()>0&&k>0){
+                result.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
+            }
+        }
+        return result;
     }
 };

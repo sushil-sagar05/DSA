ee97fad0ee082a1088585123cfbd69f43f0f40e7
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-18 23:17:40 +0530

    Time: 2 ms (66.78%), Space: 24.4 MB (45.51%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    public:
+        bool validBox(vector<vector<char>>& board, int sr, int sc, int er, int ec) {
+            unordered_set<char> st;
+            for (int i = sr; i <= er; i++) {
+                for (int j = sc; j <= ec; j++) {
+                    if (board[i][j] == '.') continue;  
+                    if (st.find(board[i][j]) != st.end()) {
+                        return false;  
+                    }
+                    st.insert(board[i][j]);
+                }
+            }
+            return true;
+        }
+    
+        bool isValidSudoku(vector<vector<char>>& board) {
+            // Validate rows
+            for (int row = 0; row < 9; row++) {
+                unordered_set<char> st;
+                for (int col = 0; col < 9; col++) {
+                    if (board[row][col] == '.') continue;  
+                    if (st.find(board[row][col]) != st.end()) {
+                        return false;  
+                    }
+                    st.insert(board[row][col]);
+                }
+            }
+    
+            // Validate columns
+            for (int col = 0; col < 9; col++) {
+                unordered_set<char> st;
+                for (int row = 0; row < 9; row++) {
+                    if (board[row][col] == '.') continue;  
+                    if (st.find(board[row][col]) != st.end()) {
+                        return false;  
+                    }
+                    st.insert(board[row][col]);
+                }
+            }
+    
+           
+            for (int sr = 0; sr < 9; sr += 3) {
+                for (int sc = 0; sc < 9; sc += 3) {
+                    if (!validBox(board, sr, sc, sr + 2, sc + 2)) {
+                        return false; 
+                    }
+                }
+            }
+    
+            
+            return true;
+        }
+    };

bad11bf53a48153983cb3f66ec08cbf0bd6635db
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-21 22:05:22 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+     s.erase(remove_if(s.begin(), s.end(), [](char c) { return !isalnum(c); }), s.end());
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        int i=0,j=s.size()-1;
+        while(i<j){
+           if (s[i]!=s[j]) return false;
+           i++;
+           j--;
+        }
+        return true;
+    }
+};

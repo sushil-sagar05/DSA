1b58dd635e3ecf6f928f23365df04cc323a53c29
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-29 13:14:40 +0530

    Time: 3 ms (66.53%), Space: 10.1 MB (45.34%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,17 +6,26 @@ public:
         if(n<m){
             return false;
         }
-         sort(begin(s1),end(s1));
-            //T.C = O(n-m)*nlogn
-            //S.C= O(n)
-        for(int i=0;i<=n-m;i++){
-            string substring = s2.substr(i,m);
-            sort(begin(substring),end(substring));
-            if(s1==substring){
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
+
+        for(char &ch :s1){
+            s1_freq[ch-'a']++;
+        }
+
+        int i=0;
+        int j=0;
+        while(j<n){
+            s2_freq[s2[j]-'a']++;
+            if(j-i+1>m){
+                s2_freq[s2[i]-'a']--;
+                i++;
+            }
+            if(s2_freq==s1_freq){
                 return true;
             }
+            j++;
         }
-         
         return false;
     }
 };

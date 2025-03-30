c75c202cd7e9cab298d4d72fc3bc1ccda0f75722
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-30 09:35:54 +0530

    Time: 0 ms (100%), Space: 13.1 MB (15.6%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -11,16 +11,24 @@ public:
                 st.push(2*st.top());
             }
             if(operations[i]=="+"){
-                int lastsum = 0;
-              int lastfirst=st.top();
-              lastsum+=lastfirst;
-                if(!st.empty()){
-                st.pop();
-                 lastsum+=st.top();
-                }
-                st.push(lastfirst);
-                st.push(lastsum);
-             }
+            //     int lastsum = 0;
+            //   int lastfirst=st.top();
+            //   lastsum+=lastfirst;
+            //     if(!st.empty()){
+            //     st.pop();
+            //      lastsum+=st.top();
+            //     }
+            //     st.push(lastfirst);
+            //     st.push(lastsum);
+            //  }
+            if(st.size()>=2){
+            int last = st.top();
+            st.pop();
+            int secondlast=st.top();
+            st.push(last);
+            st.push(last+secondlast);
+            }
+            }
            if (operations[i] == "C" || operations[i] == "D" || operations[i] == "+") {
             }
             else{

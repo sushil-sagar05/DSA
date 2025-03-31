69f8a9041db5187c9712b254d1a5f5e559b484f5
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-03-31 11:37:35 +0530

    Time: 0 ms (100%), Space: 17.3 MB (15.78%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    int operate(int a,int b,string token){
-        if(token=="+"){
-            return a+b;
-        }
-        if(token=="-"){
-            return a-b;
-        }
-        if(token=="*"){
-            return a*b;
-        }
-        if(token=="/"){
-            return a/b;
-        }
-        return -1;
-    }
+    // int operate(int a,int b,string token){
+    //     if(token=="+"){
+    //         return a+b;
+    //     }
+    //     if(token=="-"){
+    //         return a-b;
+    //     }
+    //     if(token=="*"){
+    //         return a*b;
+    //     }
+    //     if(token=="/"){
+    //         return a/b;
+    //     }
+    //     return -1;
+    // }
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
+        // declare lamba functions in unorderedmap
+        unordered_map<string, function<int (int,int)> >mp = {
+            {"+",[](int a,int b){return a+b;}},
+            {"-",[](int a,int b){return a-b;}},
+            {"*",[](int a,int b){return a*b;}},
+            {"/",[](int a,int b){return a/b;}}
+        };
        for(string &token:tokens){
          if(token=="+"||token=="-"||token=="*"||token=="/"){
             int second = st.top();
             st.pop();
             int first = st.top();
             st.pop();
-           int result =  operate(first,second,token);
+        //    int result =  operate(first,second,token);
+        int result = mp[token](first,second);
             st.push(result);
         }
         else{

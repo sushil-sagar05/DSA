30992f7eb05a0414977f0dd434f51b32c87f9bb0
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-04-03 03:22:41 +0530

    Time: 41 ms (64.26%), Space: 82.5 MB (70.57%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n=position.size();
-        stack<float> st;
+        int n= position.size();
+        stack<float>st;
         vector<pair<int,float>> vec;
         for(int i=0;i<n;i++){
+            // push the position with time of each car
             vec.push_back({position[i],float(target-position[i])/speed[i]});
         }
         sort(vec.begin(),vec.end());
-        for(auto it:vec){
-            float time = it.second;
-            while(st.size()&&(time>=st.top())){
-                st.pop();
-            }
-            st.push(time);
+       for(auto it:vec){
+        float time=it.second;
+        while(st.size()&&(time>=st.top())){
+            st.pop();
         }
-        return st.size();
+        st.push(time);
+       }
+       return st.size();
     }
 };

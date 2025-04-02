1b238832b809e17c2f717c800eefb606f4517fa8
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2025-04-02 09:20:47 +0530

    Time: 3 ms (58.87%), Space: 11.9 MB (32.65%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        string token = "";
+        stringstream ss(path);
         stack<string> st;
-        int n = path.size();
-        string result;
-
-        for (int i = 0; i < n; i++) {
-            if (path[i] == '/') {
-                continue;
-            }
-            string temp;
-            while (i < n && path[i] != '/') { 
-             temp += path[i];
-            i++;
-            }
-            
-            if (temp == "." ) {
-                continue;
-            }
-            if (temp == "..") {
-                if (!st.empty()) {
-                    st.pop();
-                }
-            } else {
-                st.push(temp);
+        while(getline(ss,token,'/')){
+            if(token==""||token==".")continue;
+            if(token!=".."){
+                st.push(token);
+            }else if(!st.empty()){
+                st.pop();
             }
         }
-        while (!st.empty()) {
-            result = "/" + st.top() + result;
+        if(st.empty()){
+            return "/";
+        }
+        string result = "";
+        while(!st.empty()){
+            result = "/"+st.top()+result;
             st.pop();
         }
-
-        return result.empty() ? "/" : result;
+        return result;
     }
 };

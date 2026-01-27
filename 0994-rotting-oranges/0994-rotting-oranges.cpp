242134615ec80562734ce3125889f889402f3991
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2026-01-27 22:41:13 +0530

    Time: 0 ms (100%), Space: 17.1 MB (44.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -16,21 +16,21 @@ public:
             }
         }
         int tm=0;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,+1,0,-1};
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
+            int row=q.front().first.first;
+            int col=q.front().first.second;
             int t=q.front().second;
             tm=max(tm,t);
             q.pop();
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
-                vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                q.push({{nrow,ncol},t+1});
-                vis[nrow][ncol]=2;
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<n &&ncol>=0 && ncol<m 
+                && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
                 }
             }
         }

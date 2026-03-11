231a6f1651a4437aa547bfa80b50134fa6d2378a
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2026-03-11 08:03:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
+    bool check(int start,int n,vector<vector<int>>& graph,int color[]){
         queue<int> q;
         q.push(0);
-        int color[n];
-        for(int i=0;i<n;i++){
-            color[i]=-1;
-        }
-        color[0]=0;
+        color[start]=0;
         while(!q.empty()){
             int node=q.front();
             q.pop();
-            for(auto it: graph[node]){
-                // if the adjacent node is not yet colored
-                // you will give opposite color to the node
-                if(color[it] == -1){
-                    color[it]= !color[node];
+            for(auto it:graph[node]){
+                if(color[it]==-1){    
+                    color[it]=!color[node];
                     q.push(it);
-                }
-                // is the adjacent guy having the same color
-                // someone did color on it some other path
-                else if (color[it]==color[node]){
+                }else if(color[it]==color[node]){
                     return false;
                 }
-            }
+                }
         }
         return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+     int n=graph.size();
+     int color[n];
+     for(int i=0;i<n;i++) color[i]=-1 ;
+     for(int i=0;i<n;i++){
+        if(color[i]==-1){
+            if(check(i,n,graph,color)==false)return false;
+        }
+     }
+     return true;
     }
 };

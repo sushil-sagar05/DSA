8dbcae7cf17bc2d5ae0163e5a843bf9f9d294e54
Sushil Sagar <169032900+sushil-sagar05@users.noreply.github.com>
2026-03-11 20:09:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (4.02%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
+        // Approach 1 T.C-> log(n)
+        // int result=0,counter=0;
+        // if(num==0) return 1;
+        // while(num){
+        //     int r=num%2;
+        //     result+=pow(2,counter)*!r;
+        //     num=num/2;
+        //     counter++;
+        // }
+        // return result;
+
+        // Approach 2 T.C-> O(1)
         int result=0,counter=0;
         if(n==0) return 1;
-        while(n){
-            int r=n%2;
-            result+=pow(2,counter)*!r;
-            n=n/2;
-            counter++;
-        }
-        return result;
+        int bits=floor(log2(n))+1;
+        int mask=pow(2,bits)-1;
+        return n^mask;
     }
 };
